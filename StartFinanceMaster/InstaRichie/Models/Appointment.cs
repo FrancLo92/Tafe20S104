01575d1367620fb7b2fe2e240dfafe60292e7b96
Francesco Lopez <francesco.lopez92@hotmail.com>
2020-03-23 20:39:11 +1030

    Appointment class created

---
+++ StartFinanceMaster/InstaRichie/Models/Appointment.cs
@@ -0,0 +1,23 @@
+﻿using SQLite.Net.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StartFinance.Models
+{
+    public class Appointment
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        [Unique]
+        public string AptmnName { get; set; }
+
+       public string AptmnDesc { get; set; }
+
+        [NotNull]
+        public DateTime AptmnDate { get; set; }
+    }
+}
